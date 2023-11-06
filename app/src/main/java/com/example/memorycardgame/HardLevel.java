319907779e9d766d2f0276fba750155467873c79
Oleksandr Lisnytskyi <oleksandrl1009@gmail.com>
2023-11-05 22:07:09 -0700

    All empty views added

---
+++ app/src/main/java/com/example/memorycardgame/HardLevel.java
@@ -0,0 +1,14 @@
+package com.example.memorycardgame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HardLevel extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hard_level);
+    }
+}
---
+++ app/src/main/java/com/example/memorycardgame/TopScore.java
@@ -0,0 +1,14 @@
+package com.example.memorycardgame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TopScore extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_top_score);
+    }
+}
