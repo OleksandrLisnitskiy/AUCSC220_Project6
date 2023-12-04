9e234b0194a4382ef974705d5366b8897f208773
eSKay7 <147770500+eSKay7@users.noreply.github.com>
2023-12-04 13:28:07 -0700

    Adaptation of design to Samsung A71 phone

--- app/src/main/java/com/example/memorycardgame/EasyLevel.java
+++ app/src/main/java/com/example/memorycardgame/EasyLevel.java
@@ -2,13 +2,8 @@ package com.example.memorycardgame;
 
 import android.os.Build;
 import android.os.Bundle;
-import android.view.Gravity;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.PopupWindow;
 
 import androidx.annotation.RequiresApi;
 
@@ -36,46 +31,6 @@ public class EasyLevel extends MainActivity {
                 CreatePopUpWindow(layout);
             }
         });
-    }
-
-    protected void onCreate2(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_easy_level);
-    }
-
-    public void onCardClick(View view) {
-        ImageView cardImage = findViewById(R.id.imageView12);
-        cardImage.setImageResource(R.drawable.mindstone);
-    }
-
-    public void onCardClick2(View view){
-        ImageView cardImage2 = findViewById(R.id.imageView13);
-        cardImage2.setImageResource(R.drawable.timestone);
-    }
-    public void onCardClick3(View view){
-        ImageView cardImage3 = findViewById(R.id.imageView14);
-        cardImage3.setImageResource(R.drawable.spacestone);
-    }
-    public void onCardClick4(View view){
-        ImageView cardImage4 = findViewById(R.id.imageView15);
-        cardImage4.setImageResource(R.drawable.realitystone);
-    }
-    public void onCardClick5(View view) {
-        ImageView cardImage5 = findViewById(R.id.imageView16);
-        cardImage5.setImageResource(R.drawable.spacestone);
-    }
-
-    public void onCardClick6(View view){
-        ImageView cardImage6 = findViewById(R.id.imageView17);
-        cardImage6.setImageResource(R.drawable.realitystone);
-    }
-    public void onCardClick7(View view){
-        ImageView cardImage7 = findViewById(R.id.imageView18);
-        cardImage7.setImageResource(R.drawable.mindstone);
-    }
-    public void onCardClick8(View view){
-        ImageView cardImage8 = findViewById(R.id.imageView19);
-        cardImage8.setImageResource(R.drawable.timestone);
     }
 
 
