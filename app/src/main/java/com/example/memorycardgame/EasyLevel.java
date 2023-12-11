3b2663dd90b4e018cd84b435e36a4d5d65697319
eSKay7 <147770500+eSKay7@users.noreply.github.com>
2023-12-10 22:29:10 -0700

    Revert "Fixed Congratulations, Loosing, pause window  xml for phone. Completed documentation for easy, medium and hard level."
    This reverts commit c738c6a2478a5dcaa4bbc46afd09a4c837bf7957.

--- app/src/main/java/com/example/memorycardgame/EasyLevel.java
+++ app/src/main/java/com/example/memorycardgame/EasyLevel.java
@@ -31,8 +31,7 @@ public class EasyLevel extends MainActivity {
         matchedPairs = 0;
         setContentView(R.layout.activity_easy_level);
         layout = findViewById(R.id.linearLayoutEasy);
-        // calls method to start level timer.
-        startLevelTimer(false);
+        startLevelTimer(false);// calls method to start level timer
         ImageView pauseButton = findViewById(R.id.pauseButton);
         imageViewIds = getImageViewIds(4, 2);
         TextView Score = findViewById(R.id.scoreEasyLevel);
@@ -62,15 +61,9 @@ public class EasyLevel extends MainActivity {
                     public void onClick(View v) {
                         //Used to get the imageResourceId of the card clicked.
                         int imageResourceId = getResources().getIdentifier(game.cardBoard[finalI][finalJ].getImagePath(), "drawable", getPackageName());
-
-                        /*
-                        Set ImageResource of the card clicked and then disables the card to avoid it
-                         from being clicked again.
-                         */
                         InfinitStone.setImageResource(imageResourceId);
                         InfinitStone.setEnabled(false);
                         List<Integer> coordinates = new ArrayList<>();
-                        //Add coordinates of the card clicked to Array.
                         coordinates.add(finalI);
                         coordinates.add(finalJ);
                         game.flipCounterDict.add(coordinates);
@@ -78,28 +71,17 @@ public class EasyLevel extends MainActivity {
                         String packageName = getPackageName();
                         new Handler().postDelayed(new Runnable() {
                             @Override
-                            /**
-                             * The following method is used to run the game mechanics.
-                             */
                             public void run() {
                                 if (game.flipCounterDict.size() >= 2) {
 
                                     List<Integer> card1 = game.flipCounterDict.get(0);
                                     List<Integer> card2 = game.flipCounterDict.get(1);
-                                    //Checks if the images on the 2 cards match.
                                     if(game.cardBoard[card1.get(0)][card1.get(1)].getImagePath().equals(
                                             game.cardBoard[card2.get(0)][card2.get(1)].getImagePath())){
-                                        //If they do match then score is updated as a successful try.
                                         Score.setText("Score:  " + game.score.successfulTry());
 
-                                        //Get the ID's of the matched cards.
                                         String imageViewIdName = "imageView" + String.valueOf(card1.get(0)) + (card1.get(1));
                                         int imageViewId = resources.getIdentifier(imageViewIdName, "id", packageName);
-
-                                        /*
-                                        Helps disable click listeners for both cards matched so they
-                                        can't be clicked again.
-                                         */
                                         ImageView card = findViewById(imageViewId);
                                         card.setEnabled(false);
                                         card.setImageResource(R.drawable.empty_background);
@@ -120,30 +102,19 @@ public class EasyLevel extends MainActivity {
                                             game.quit(timeToComplete);
                                         }
                                     }
-                                    /*
-                                    Is executed when the cards don't match.
-                                    Executes a failed try scenario where score is updated for a
-                                    failed try. Unmatched cards are flipped back and click listener
-                                    is enabled.
-                                     */
                                     else {
                                         Score.setText("Score:  " + game.score.failedTry());
 
-                                        //Getting ID's of unmatched cards.
                                         String imageViewIdName = "imageView" + String.valueOf(card1.get(0)) + (card1.get(1));
                                         int imageViewId = resources.getIdentifier(imageViewIdName, "id", packageName);
-                                        //Flip back unmatched card.
                                         ImageView card = findViewById(imageViewId);
                                         card.setImageResource(R.drawable.card_for_easy_level);
-                                        //Enabling the click listeners for first card.
                                         card.setEnabled(true);
                                         imageViewIdName = "imageView" + String.valueOf(card2.get(0)) + (card2.get(1));
                                         imageViewId = resources.getIdentifier(imageViewIdName, "id", packageName);
                                         card = findViewById(imageViewId);
                                         card.setImageResource(R.drawable.card_for_easy_level);
-                                        //Enabling the click listeners for second card.
                                         card.setEnabled(true);
-                                        //Resets the flipCounterDict for the next flip.
                                         game.flipCounterDict = new ArrayList<>();
                                     }
                                 }
@@ -160,10 +131,6 @@ public class EasyLevel extends MainActivity {
     protected void onStart() {
         super.onStart();
 
-        /*
-        Delays execution to show the card images after 500 milliseconds of the game starting by
-        running a nested for loop.
-        */
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -179,14 +146,13 @@ public class EasyLevel extends MainActivity {
         }, 500);
 
 
-        //Delays the execution to hide the card images after 1500 milliseconds by running a nested for loop.
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 for (int i = 0; i < 4; i++) {
                     for (int j = 0; j < 2; j++) {
                         ImageView InfinitStone = findViewById(imageViewIds.get(i).get(j));
-                        //Sets the card image to a default back of the card image.
+
                         InfinitStone.setImageResource(R.drawable.card_for_easy_level);
                     }
                 }
